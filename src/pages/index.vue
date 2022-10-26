0e66c518b33ef0c1f577434d2f7faa096f764476
Valentin25260 <95018311+Valentin25260@users.noreply.github.com>
2022-10-26 23:34:53 +0200

    ajustement intégration index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import Boutton from "@/components/boutton.vue";
+import Boutton from "@/components/Button.vue";
 </script>
 
 <template>
   <div class=" flex gap-40 justify-center mt-30 ">
     <div class="font-poppins col-span-2 ml-36 mt-40 col-start-2">
       <h1 class="text-4xl font-medium ">
-        PERSONNALISE<br>
+        <span class="text-blue-400">PERSONNALISE</span><br>
         TA MONTRE CONNECTÉE.
       </h1>
       <h2 class="font-light text-xl mt-12">SIMPLICITÉ FACILITÉ RAPIDITÉ</h2>
@@ -22,26 +22,27 @@ import Boutton from "@/components/boutton.vue";
         fermoir n’y attache.
       </p>
 
-      <RouterLink to="/montre/new" class=" row-start-2 col-start-2 col-span-1 mt-10">
-        <Boutton />
+      <RouterLink to="personaliser" class=" row-start-2 col-start-2 col-span-1 mt-10">
+        <div class="flex justify-center items-center relative gap-2.5  mt-10 w-80">
+          <Boutton name="PERSONNALISER" />
+        </div>
       </RouterLink>
 
     </div>
-    <img src="../../public/montrenoir.png" alt="montre noir" class="mt-32">
+    <img src="../../public/images/montrenoir.png" alt="montre noir" class="mt-32">
   </div>
 
   <div class="grid grid-cols-5 ">
-    <img src="../../public/montrebleu.png" alt="montre bleu" class="mt-32 flex-auto w-full ">
-    <img src="../../public/montrejaune.png" alt="montre jaune" class="mt-32 flex-auto w-full ">
-    <img src="../../public/montreviolette.png" alt="montre violette" class="mt-32 flex-auto w-full ">
-    <img src="../../public/montreverte.png" alt="montre verte" class="mt-32 flex-auto w-full ">
-    <img src="../../public/montreorange.png" alt="montre orange" class="mt-32 flex-auto w-full ">
+    <img src="../../public/images/montrebleu.png" alt="montre bleu" class="mt-32 flex-auto w-full ">
+    <img src="../../public/images/montrejaune.png" alt="montre jaune" class="mt-32 flex-auto w-full ">
+    <img src="../../public/images/montreviolette.png" alt="montre violette" class="mt-32 flex-auto w-full ">
+    <img src="../../public/images/montreverte.png" alt="montre verte" class="mt-32 flex-auto w-full ">
+    <img src="../../public/images/montreorange.png" alt="montre orange" class="mt-32 flex-auto w-full ">
   </div>
-
 
   <div class=" flex gap-36 justify-center items-center mt-30 ">
 
-    <img src="../../public/montreblanche.png" alt="montre blanche" class="mt-32">
+    <img src="../../public/images/montreblanche.png" alt="montre blanche" class="mt-32">
 
     <div class="font-poppins col-span-2  mt-40 col-start-2">
       <h1 class="text-4xl font-medium ">
@@ -59,8 +60,4 @@ import Boutton from "@/components/boutton.vue";
       </p>
     </div>
   </div>
-
-
-
-
 </template>
