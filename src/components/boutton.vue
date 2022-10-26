2e4acd980b334cf3b48e15b46095ef618eb43faa
Valentin25260 <95018311+Valentin25260@users.noreply.github.com>
2022-10-26 23:57:28 +0200

    renommage + modifcation interface bouton

--- src/components/boutton.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="flex justify-center bg-blue-400 items-center relative gap-2.5 px-5 py-4 mt-10 w-80">
-        <p class="flex-grow-0 flex-shrink-0 text-3x1 text-center uppercase text- font-poppins ">
-            Personnaliser</p>
-    </div>
-</template>
---
+++ src/components/button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+    name?: string;
+}>();
+</script>
+
+<template>
+    <button
+        class=" px-5 py-4 bg-blue-400 flex-grow-0 text-white flex-shrink-0 text-3x1 text-center uppercase NeutralFace">{{
+        name }}</button>
+</template>
