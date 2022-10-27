a27820e07833b2b45c1e43ac3d003e0d8e2b71e0
Valentin25260 <95018311+Valentin25260@users.noreply.github.com>
2022-10-27 13:53:12 +0200

    correction index.css

--- src/components/button.vue
+++ src/components/button.vue
@@ -6,6 +6,7 @@ defineProps<{
 
 <template>
     <button
-        class=" px-5 py-4 bg-blue-400 flex-grow-0 text-white flex-shrink-0 text-3x1 text-center uppercase NeutralFace">{{
-        name }}</button>
+        class=" px-5 py-4 bg-blue-400 flex-grow-0 text-white flex-shrink-0 text-3x1 text-center uppercase font-NeutralFace">{{
+        name
+        }}</button>
 </template>
